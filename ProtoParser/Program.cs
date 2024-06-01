c08b85227cae54bbef51bf979fd309a193c8f65e
StachedCrusader <ray@dicec.co>
2024-06-01 17:49:03 -0400

    Have a working serializer/deserializer flow. Going to work on setting up pubsub emulator next

--- ProtoParser/Program.cs
+++ ProtoParser/Program.cs
@@ -1,21 +1,39 @@
 ﻿using System;
+using System.Text.Json;
+using Google.Protobuf;
 using Proto.Messages;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("What do you want? s = serialize json into proto, d = deserialize from proto to json, a = sample message structure");
+Console.WriteLine("What do you want? s = serialize json into proto (one line plz), d = deserialize from proto to json, a = sample message structure");
 
 string input = Console.ReadLine()!;
 
 if(input.StartsWith("s"))
 {
+    string jsonInput = Console.ReadLine()!;
 
+    Person personFromJson = Person.Parser.ParseJson(jsonInput);
 
+    byte[] serializedPerson = personFromJson.ToByteArray();
+
+    Console.WriteLine(Convert.ToBase64String(serializedPerson));
 }
 else if (input.StartsWith("d"))
 {
+    string protoInput = Console.ReadLine()!;
 
+    byte[] serializedAsBytes = Convert.FromBase64String(protoInput);
+
+    Person personFromProto = Person.Parser.ParseFrom(serializedAsBytes);
+
+    Console.WriteLine(personFromProto);
 }
 else if(input.StartsWith("a"))
 {
-
+    Console.WriteLine(new Person()
+    {
+        FirstName = "First Name",
+        LastName = "Last Name",
+        Id = 1234
+    });
 }
