055952f040175ca2f025a438612980ad0e3262a2
StachedCrusader <ray@dicec.co>
2024-06-01 16:53:40 -0400

    Made a protoparse. Need to figure out how to use the generated protospec

---
+++ ProtoParser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Proto.Messages;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What do you want? s = serialize json into proto, d = deserialize from proto to json, a = sample message structure");
+
+string input = Console.ReadLine()!;
+
+if(input.StartsWith("s"))
+{
+
+
+}
+else if (input.StartsWith("d"))
+{
+
+}
+else if(input.StartsWith("a"))
+{
+
+}
